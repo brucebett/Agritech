51e5c8915bb960ad09b2a27311df9b54f5765a25
brucebett <bett6448@gmail.com>
2024-11-15 23:47:19 +0300

    fri15  commit

--- agritech/settings.py
+++ agritech/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'agritech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
