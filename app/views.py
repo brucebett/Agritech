1e70591821948f763f93555f928682c30b76d991
brucebett <bett6448@gmail.com>
2024-11-17 21:36:23 +0300

    sunn4 commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from app.models import Contact
 
 # Create your views here.
 def index(request):
@@ -11,4 +13,18 @@ def about(request):
     return render(request, "about.html")
 
 def contact(request):
+    if request.method=="POST":
+        fname=request.POST.get('name')
+        femail=request.POST.get('email')
+        fsubject=request.POST.get('subject')
+        fmessage=request.POST.get('message')
+        query=Contact(name=fname,email=femail,subject=fsubject,message=fmessage)
+        query.save()
+        messages.success(request, "Thanks for contacting us.We will get to you Soon!")
+
+        return redirect('/contact')
+
     return render(request, "contact.html")
+
+
+
