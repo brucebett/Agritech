6c5e3745e1b8a530e2b51917c06ff1facb1463c4
brucebett <bett6448@gmail.com>
2024-11-17 19:54:17 +0300

    sunn1  commit

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = [
 path('',views.index,name='index'),
 path('service/',views.services,name='service'),
-path('about/',views.about,name='about')
+path('about/',views.about,name='about'),
+path('contact/',views.contact,name='contact')
 ]
--- app/views.py
+++ app/views.py
@@ -9,3 +9,6 @@ def services(request):
 
 def about(request):
     return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
